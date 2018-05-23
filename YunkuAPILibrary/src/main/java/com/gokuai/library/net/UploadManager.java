26bc4f51b5cd46363c8e8594f272cddc7f0f56bb
wink <wink@gokuai.cn>
2018-05-23 15:10:44 +0800

    修复异步上传

--- YunkuAPILibrary/src/main/java/com/gokuai/library/net/UploadManager.java
+++ YunkuAPILibrary/src/main/java/com/gokuai/library/net/UploadManager.java
@@ -97,7 +97,7 @@ public class UploadManager {
     }
 
     private void doUploadAsync() {
-        Thread thread = new Thread(new Runnable() {
+        new Thread(new Runnable() {
             @Override
             public void run() {
                 try {
@@ -119,8 +119,7 @@ public class UploadManager {
                     }
                 }
             }
-        });
-        thread.run();
+        }).start();
     }
 
     private void startUpload() throws YunkuException, IOException {
